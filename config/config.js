f1528eb161cccbed99a41606fe257190be979954
Ed <ed@polarity.io>
2019-04-16 14:02:28 -0400

    Change log level to info

--- config/config.js
+++ config/config.js
@@ -80,6 +80,6 @@ module.exports = {
         proxy: ''
     },
     logging: {
-        level: 'trace',  //trace, debug, info, warn, error, fatal
+        level: 'info',  //trace, debug, info, warn, error, fatal
     }
 };
